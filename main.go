f58fc14f4afd1ab0e22d74eddcc8581923425b82
Naoki Kimura <n.kimura.cap@gmail.com>
2021-02-27 17:05:55 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+    "crypto/sha1"
     "fmt"
+    "io"
     "math/rand"
 )
 
 func main() {
     fmt.Println("Hello, World!")
+
     fmt.Println(rand.Intn(100))
+
     password := "L8pgrXvde@CGm8b!mE4"
     fmt.Println(password)
+
+    h := sha1.New()
+    io.WriteString(h, "His money is twice tainted:")
+    fmt.Printf("% x", h.Sum(nil))
 }
